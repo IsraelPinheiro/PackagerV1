00a0f3b1fa236c47abbc3a971a13de88dc4c26c8
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-08 23:01:07 -0300

    Add faker seeder

---
+++ database/seeds/faker/FakeUsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Factory;
+use App\User as User;
+
+class FakeUsersTableSeeder extends Seeder{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        $faker = Factory::create('pt_BR');
+        $max = rand(10, 60);
+        for($i = 0; $i <= $max; $i++){
+            $perfil = rand(1, 3);
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make($faker->password),
+                'profile_id' => $perfil,
+                'created_by' => 1
+            ]);
+        }
+        $this->command->info($max.' Test Users created');
+    }
+}
