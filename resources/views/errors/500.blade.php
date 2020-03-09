c8aac5e5cf1efb26b125e3196a663e9a5ebaa8bd
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-08 23:49:41 -0300

    Add background image for some error pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,3 +3,6 @@
 @section('title', __('Server Error'))
 @section('code', '500')
 @section('message', __('Server Error'))
+@section('image')
+    <div style="background-image: url({{ asset('/svg/500.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center"></div>
+@endsection
