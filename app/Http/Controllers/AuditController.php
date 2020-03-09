5a8bc035b6076dd4889ba1eb1580f9dc68d831d7
israel reis pinheiro <1818210>
2020-03-09 19:54:06 -0300

    Unify Change and Access logs controllers

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AccessLogController extends Controller{
+class AuditController extends Controller{
     /**
      * Display a listing of the resource.
      *
