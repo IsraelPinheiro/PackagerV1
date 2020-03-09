abd5a52f74538c00b7dd1d011d9b7cb0673349f5
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-08 22:15:42 -0300

    Add storage controll columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ class CreateUsersTable extends Migration{
      */
     public function up(){
         Schema::create('users', function (Blueprint $table){
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration{
             $table->bigInteger('profile_id')->unsigned();
             $table->boolean('active')->default(true);
 
+            $table->boolean('override_storage_limits')->default(false);     //If true, the local storage limitters will be used, instead of the ones set on the user profile
             $table->bigInteger('max_file_size')->unsigned()->default(0);    //Max size (In Bytes) of a given file. A value of 0 means there is no limit
             $table->bigInteger('max_package_size')->unsigned()->default(0); //Max total size (In Bytes) of a given package. A value of 0 means there is no limit
             $table->bigInteger('max_storage_size')->unsigned()->default(0); //The storage limit of the user (In Bytes). A value of 0 means there is no limit
