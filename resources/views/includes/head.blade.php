1f2762927485e11082b0d4e0613be912f7f6327c
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-11 20:48:58 -0300

    Add svg based favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,8 +1,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}" color="#990000">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
