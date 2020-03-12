d987947100037ca38b97659bd920d11744a6401a
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-11 21:40:44 -0300

    cast accessLog values

--- app/AccessLog.php
+++ app/AccessLog.php
@@ -5,5 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class AccessLog extends Model{
-    //
+    const CREATED_AT = 'accessed_at';
+    protected $casts = [
+        'accessed_at' => 'datetime',
+    ];
 }
