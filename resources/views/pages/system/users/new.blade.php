f36897c29b75ed0c941719631093a6c6fd3b3707
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-15 21:29:24 -0300

    Structure for the user creation modal

---
+++ resources/views/pages/system/users/new.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.modal')
+@section('title','Novo Usuário')
+@section('content')
+	<form id="FormModal">
+		@csrf
+        {{-- Nome e Email --}}
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="Nome">Nome</label>
+					<input name="Nome" type="text" class="form-control" required>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="Email">Email</label>
+					<input name="Email" type="email" class="form-control" required>
+				</div>
+			</div>
+		</div>
+        {{-- Perfil --}}
+		<div class="row">
+			<div class="col-md-6">
+                <div class="form-group">
+                    <label for="Perfil">Perfil</label>
+                    <select name="Perfil" class="form-control">
+                        <option selected disabled>Selecione um Perfil</option>
+                        @foreach ($profiles as $profile)
+                            <option value="{{$profile->id}}">{{$profile->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+			</div>
+		</div>
+        {{-- Senha e Confirmação --}}
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="Password">Senha</label>
+					<input name="Password" id="Password" type="password" class="form-control" minlength="6" required>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="Password_confirmation">Confirmar Senha</label>
+					<input name="Password_confirmation" id="Password_confirmation" type="password" class="form-control" required>
+				</div>
+			</div>
+		</div>
+	</form>
+@stop
+@section('actions')
+	<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+	<button type="submit" class="btn btn-primary btn-users-store">Salvar</button>
+@stop
