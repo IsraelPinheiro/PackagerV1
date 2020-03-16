0833a5aa77bd393b2f2865258b18a84a6be84d09
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-15 23:31:07 -0300

    AlertifyJs substituted by Sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,16 @@
 require('./bootstrap');
 require('./sb-admin-2')
 require('chart.js')
+import swal from 'sweetalert';
 require('datatables')
 require('datatables.net-bs4')
-require('./datatables.init')
 require('jquery-mask-plugin')
 require('jquery.easing')
 
+//Datatables Initialisation
+require('./datatables.init')
+
 //Page specific code
 require('./pages/common')
-var users = require('./pages/users')
+require('./pages/users')
+require('./pages/profiles')
