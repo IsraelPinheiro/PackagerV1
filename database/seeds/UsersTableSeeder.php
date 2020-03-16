b00a78a4232a9a3f1ea8adbfaef867b489b00c6e
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-15 23:33:49 -0300

    Code cleaning

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,7 +11,6 @@ class UsersTableSeeder extends Seeder{
      */
     public function run(){
         DB::table('users')->truncate();
-        
         User::create([
             'name' => 'Administrador',
             'email' => 'admin@packager.com.br',
