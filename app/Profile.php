621d509d4a9dfef0cb3ef0b31806d5e2f969e804
Israel R Pinheiro <israel.pinheiro@live.com>
2020-03-15 23:57:20 -0300

    Add relationships

--- app/Profile.php
+++ app/Profile.php
@@ -5,5 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model{
-    //
+    public function users(){
+        return $this->hasMany('App\User');
+    }
+    public function logs(){
+        return $this->morphMany('App\ChangeLog', 'loggable');
+    }
 }
