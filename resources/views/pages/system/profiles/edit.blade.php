baa144efee96f6a614e808c40540fe67aacec038
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-16 13:08:01 -0300

    add data load logic

--- resources/views/pages/system/profiles/edit.blade.php
+++ resources/views/pages/system/profiles/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.modal')
-@section('title','Novo Perfil de Usuário')
+@section('title',$profile->name)
 @section('modalclass','modal-lg')
 @section('content')
 	<form id="FormModal">
@@ -9,7 +9,7 @@
 			<div class="col-md-12">
 				<div class="form-group">
 					<label for="Name">Nome</label>
-					<input name="Name" type="text" class="form-control" required>
+					<input name="Name" type="text" class="form-control" value="{{ $profile->name }}" required>
 				</div>
 			</div>
 		</div>
@@ -18,7 +18,7 @@
 			<div class="col-md-12">
 				<div class="form-group">
 					<label for="Description">Descrição</label>
-					<textarea name="Description" class="form-control" rows="4"></textarea>
+					<textarea name="Description" class="form-control" rows="4">{{ $profile->description }}</textarea>
 				</div>
 			</div>
 		</div>
@@ -28,19 +28,19 @@
 			<div class="col-md-4">
 				<div class="form-group">
 					<label for="max_file_size">Tamanho Máximo por arquivo</label>
-					<input name="max_file_size" type="number" value="0" min="0" class="form-control" required>
+					<input name="max_file_size" type="number" value="{{ $profile->max_file_size }}" min="0" class="form-control" required>
 				</div>
 			</div>
             <div class="col-md-4">
 				<div class="form-group">
 					<label for="max_package_size">Tamanho Máximo por Pacote</label>
-					<input name="max_package_size" type="number" value="0" min="0" class="form-control" required>
+					<input name="max_package_size" type="number" value="{{ $profile->max_package_size }}" min="0" class="form-control" required>
 				</div>
 			</div>
             <div class="col-md-4">
 				<div class="form-group">
 					<label for="max_storage_size">Capacidade de Armazenamento</label>
-					<input name="max_storage_size" type="number" value="0" min="0" class="form-control" required>
+					<input name="max_storage_size" type="number" value="{{ $profile->max_storage_size }}" min="0" class="form-control" required>
 				</div>
 			</div>
 		</div>
@@ -60,11 +60,11 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="relatorios_operacionais_read" id="relatorios_operacionais_read" type="checkbox" hidden>
+							<input name="relatorios_operacionais_read" id="relatorios_operacionais_read" type="checkbox" @if(json_decode($profile->acl_reports_operational)->read) checked @endif hidden>
 							<label for="relatorios_operacionais_read">Relatórios Operacionais</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="relatorios_administrativos_read" id="relatorios_administrativos_read" type="checkbox" hidden>
+							<input name="relatorios_administrativos_read" id="relatorios_administrativos_read" type="checkbox" @if(json_decode($profile->acl_reports_administrative)->read) checked @endif hidden>
 							<label for="relatorios_administrativos_read">Relatórios Administrativos</label>
 						</div>
 					</div>
@@ -85,11 +85,11 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="dashboards_gerencial_read" id="dashboards_gerencial_read" type="checkbox" hidden>
+							<input name="dashboards_gerencial_read" id="dashboards_gerencial_read" type="checkbox" @if(json_decode($profile->acl_dashboards_management)->read) checked @endif hidden>
 							<label for="dashboards_gerencial_read">Dashboard Gerencial</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="dashboards_operacional_read" id="dashboards_operacional_read" type="checkbox" hidden>
+							<input name="dashboards_operacional_read" id="dashboards_operacional_read" type="checkbox" @if(json_decode($profile->acl_dashboards_operational)->read) checked @endif hidden>
 							<label for="dashboards_operacional_read">Dashboard Operacional</label>
 						</div>
 					</div>
@@ -116,11 +116,11 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="audit_access_read" id="audit_access_read" type="checkbox" hidden>
+							<input name="audit_access_read" id="audit_access_read" type="checkbox" @if(json_decode($profile->acl_audit_accessLogs)->read) checked @endif hidden>
 							<label for="audit_access_read">Visualizar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="audit_access_download" id="audit_access_download" type="checkbox" hidden>
+							<input name="audit_access_download" id="audit_access_download" type="checkbox" @if(json_decode($profile->acl_audit_accessLogs)->download) checked @endif hidden>
 							<label for="audit_access_download">Download</label>
 						</div>
 					</div>
@@ -136,11 +136,11 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="audit_change_read" id="audit_change_read" type="checkbox" hidden>
+							<input name="audit_change_read" id="audit_change_read" type="checkbox" @if(json_decode($profile->acl_audit_changeLogs)->read) checked @endif hidden>
 							<label for="audit_change_read">Visualizar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="audit_change_download" id="audit_change_download" type="checkbox" hidden>
+							<input name="audit_change_download" id="audit_change_download" type="checkbox" @if(json_decode($profile->acl_audit_changeLogs)->download) checked @endif hidden>
 							<label for="audit_change_download">Download</label>
 						</div>
 					</div>
@@ -167,19 +167,19 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="users_read" id="users_read" type="checkbox" hidden>
+							<input name="users_read" id="users_read" type="checkbox" @if(json_decode($profile->acl_users)->read) checked @endif hidden>
 							<label for="users_read">Visualizar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="users_create" id="users_create" type="checkbox" hidden>
+							<input name="users_create" id="users_create" type="checkbox" @if(json_decode($profile->acl_users)->create) checked @endif hidden>
 							<label for="users_create">Criar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="users_update" id="users_update" type="checkbox" hidden>
+							<input name="users_update" id="users_update" type="checkbox" @if(json_decode($profile->acl_users)->update) checked @endif hidden>
 							<label for="users_update">Editar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="users_delete" id="users_delete" type="checkbox" hidden>
+							<input name="users_delete" id="users_delete" type="checkbox" @if(json_decode($profile->acl_users)->delete) checked @endif hidden>
 							<label for="users_delete">Excluir</label>
 						</div>
 					</div>
@@ -195,19 +195,19 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="profiles_read" id="profiles_read" type="checkbox" hidden>
+							<input name="profiles_read" id="profiles_read" type="checkbox" @if(json_decode($profile->acl_profiles)->read) checked @endif hidden>
 							<label for="profiles_read">Visualizar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="profiles_create" id="profiles_create" type="checkbox" hidden>
+							<input name="profiles_create" id="profiles_create" type="checkbox" @if(json_decode($profile->acl_profiles)->create) checked @endif hidden>
 							<label for="profiles_create">Criar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="profiles_update" id="profiles_update" type="checkbox" hidden>
+							<input name="profiles_update" id="profiles_update" type="checkbox" @if(json_decode($profile->acl_profiles)->update) checked @endif hidden>
 							<label for="profiles_update">Editar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="profiles_delete" id="profiles_delete" type="checkbox" hidden>
+							<input name="profiles_delete" id="profiles_delete" type="checkbox" @if(json_decode($profile->acl_profiles)->delete) checked @endif hidden>
 							<label for="profiles_delete">Excluir</label>
 						</div>
 					</div>
@@ -223,19 +223,19 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
                         <div class="checkbox checkbox-primary pr-2">
-							<input name="backups_create" id="backups_create" type="checkbox" hidden>
+							<input name="backups_create" id="backups_create" type="checkbox" @if(json_decode($profile->acl_backups)->create) checked @endif hidden>
 							<label for="backups_create">Gerar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="backups_read" id="backups_read" type="checkbox" hidden>
+							<input name="backups_read" id="backups_read" type="checkbox" @if(json_decode($profile->acl_backups)->read) checked @endif hidden>
 							<label for="backups_read">Visualizar</label>
 						</div>
                         <div class="checkbox checkbox-primary pr-2">
-							<input name="backups_restore" id="backups_restore" type="checkbox" hidden>
+							<input name="backups_restore" id="backups_restore" type="checkbox" @if(json_decode($profile->acl_backups)->restore) checked @endif hidden>
 							<label for="backups_restore">Restaurar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="backups_delete" id="backups_delete" type="checkbox" hidden>
+							<input name="backups_delete" id="backups_delete" type="checkbox" @if(json_decode($profile->acl_backups)->delete) checked @endif hidden>
 							<label for="backups_delete">Excluir</label>
 						</div>
 					</div>
@@ -251,11 +251,11 @@
 				<div class="col-md-12">
 					<div class="form-group form-inline">
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="config_read" id="config_read" type="checkbox" hidden>
+							<input name="config_read" id="config_read" type="checkbox" @if(json_decode($profile->acl_config)->read) checked @endif hidden>
 							<label for="config_read">Visualizar</label>
 						</div>
 						<div class="checkbox checkbox-primary pr-2">
-							<input name="config_update" id="config_update" type="checkbox" hidden>
+							<input name="config_update" id="config_update" type="checkbox" @if(json_decode($profile->acl_config)->update) checked @endif hidden>
 							<label for="config_update">Editar</label>
 						</div>
 					</div>
@@ -266,5 +266,5 @@
 @stop
 @section('actions')
 	<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-	<button type="submit" class="btn btn-primary btn-profiles-store">Salvar</button>
+	<button type="submit" class="btn btn-primary btn-profiles-update" data-id={{$profile->id}}>Salvar</button>
 @stop
