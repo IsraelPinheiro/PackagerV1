a7c9f4e2cbeb355c3f366c1ece81dc9f92933369
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-17 14:19:20 -0300

    add sender and recipient relations and cast expires_at to datetime

--- app/Package.php
+++ app/Package.php
@@ -5,7 +5,17 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model{
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
     public function files(){
         return $this->hasMany('App\File');
+    }
+    public function sender(){
+        return $this->belongsTo('App\User', 'sender_id');
+    }
+    public function recipient(){
+        return $this->belongsTo('App\User', 'recipient_id');
     }
 }
