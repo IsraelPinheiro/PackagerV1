05fb578edf4532df78a655c0e47dd90a14481e35
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-16 22:35:49 -0300

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -32,14 +32,24 @@ Route::get('/', function (){
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
 //Resource Routes
-Route::resource('audit', 'AuditController')->middleware('auth');    //Access Logs and Change Logs
+Route::get('/audit/{type}', 'AuditController@index')->where("type","access|change")->name('audit.index')->middleware('auth');
+Route::get('/audit/{type}/download', 'AuditController@download')->where("type","access|change")->name('audit.download')->middleware('auth');
 Route::resource('backups', 'BackupController')->middleware('auth');
 Route::resource('packages', 'PackageController')->except(['index'])->middleware('auth');
 Route::resource('profiles', 'ProfileController')->middleware('auth');
+Route::get('/profiles/{id}/users','ProfileController@listUsers')->name("profiles.users")->middleware('auth');
 Route::resource('users', 'UserController')->middleware('auth');
 Route::resource('inbounds', 'InboundPackagesController')->only(['index'])->middleware('auth');
 Route::resource('outbounds', 'OutboundPackagesController')->only(['index'])->middleware('auth');
-
-Route::resource('reports', 'OutboundPackagesController')->only(['index'])->middleware('auth');
-Route::resource('dashboard', 'OutboundPackagesController')->only(['index'])->middleware('auth');
+Route::resource('reports', 'ReportController@index')->only(['index'])->middleware('auth');
+Route::get('/reports/{type?}/{report?}', 'ReportController@index')->where("type","operational|administrative")->middleware('auth');
+Route::get('/dashboards/{type}', 'DashboardController@index')->where("type","management|operational")->name('dashboards')->middleware('auth');
 Route::resource('config', 'OutboundPackagesController')->only(['index'])->middleware('auth');
+
+//Opções
+Route::get('options/config', 'UserController@promptConfig')->name("user.config")->middleware('auth');
+Route::post('options/config', 'UserController@updateConfig')->name("user.config")->middleware('auth');
+Route::get('options/password', 'UserController@promptPassword')->name("user.password")->middleware('auth');
+Route::post('options/password', 'UserController@updatePassword')->name("user.password")->middleware('auth');
+Route::view('options/help', 'pages.options.help')->name("help")->middleware('auth');
+Route::view('options/about', 'pages.options.about')->name("about");
