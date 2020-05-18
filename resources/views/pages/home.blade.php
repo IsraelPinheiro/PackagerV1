462b931922907e2c89d6001fc5b0deb770000afe
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-18 15:48:54 -0300

    Remove aditional container-fluid div

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.app')
 @section('content')
-	<div class="container-fluid">
-        <div class="row">
-			<div class="col-xl-3 col-md-6 mb-4">
-            	
-            </div>
-			<div class="col-xl-3 col-md-6 mb-4">
-            	
-            </div>
-            <div class="col-xl-3 col-md-6 mb-4">
-            	
-            </div>
-            <div class="col-xl-3 col-md-6 mb-4">
-            	
-            </div>
-			<div class="col-xl-3 col-md-6 mb-4">
-            	
-            </div>
-		</div>
-	</div>
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            
+        </div>
+    </div>
 @stop
