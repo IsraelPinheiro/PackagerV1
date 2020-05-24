38d11761509f504a0089f9038eb50b45887f84cc
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-24 15:26:37 -0300

    Set correct classes and permissions

--- resources/views/pages/system/backups/index.blade.php
+++ resources/views/pages/system/backups/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 	<div class="page-header mb-3">
 		<h1 class="d-inline">Backups do Sistema</h1>
-		@if(json_decode(Auth::user()->profile->acl_users)->create)
-			<button class="btn btn-primary btn-circle btn-users-add float-right d-inline" title="Novo" type="button">
+		@if(json_decode(Auth::user()->profile->acl_backups)->create)
+			<button class="btn btn-primary btn-circle btn-backups-run float-right d-inline" title="Novo Backup Manual" type="button">
 				<i class="fas fa-plus"></i>
 			</button>
 		@endif
@@ -16,9 +16,7 @@
 					<th><center>Nome</center></th>
 					<th><center>Data</center></th>
 					<th><center>Hash MD5</center></th>
-					@if($userPermissions->download || $userPermissions->restore || $userPermissions->delete)
-						<th class="noorder"></th>
-					@endif
+					<th class="noorder"></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -28,21 +26,20 @@
 						<td><center>{{ $backup->name }}</center></td>
 						<td><center>{{ $backup->created_at->format('d/m/Y') }}</center></td>
                         <td><center>{{ $backup->md5 }}</center></td>
-						@if($userPermissions->download || $userPermissions->restore || $userPermissions->delete)
-							<td class="toolbox">
-								<center>
-									@if($userPermissions->download)
-										<i data-id={{$backup->id}} class="fas fa-edit fa-lg btn-backup-download pr-1" title="Editar"></i>
-                                    @endif
-                                    @if($userPermissions->restore)
-                                        <i data-id={{$backup->id}} class="fas fa-eye fa-lg btn-backup-restore pr-1" title="Exibir"></i>
-									@endif
-									@if($userPermissions->delete)
-										<i data-id={{$backup->id}} class="fas fa-trash-alt fa-lg btn-restore-del text-danger" title="Excluir"></i>
-									@endif
-								</center>
-							</td>
-						@endif
+						<td class="toolbox">
+							<center>
+								<i data-id={{$backup->id}} class="fas fa-eye fa-lg btn-backups-show pr-1" title="Exibir"></i>
+								@if($userPermissions->download)
+									<i data-id={{$backup->id}} class="fas fa-download fa-lg btn-backups-download pr-1" title="Download"></i>
+								@endif
+								@if($userPermissions->restore)
+									<i data-id={{$backup->id}} class="fas fa-undo fa-lg btn-backups-restore pr-1 disabled" title="Restaurar Backup"></i>
+								@endif
+								@if($userPermissions->delete)
+									<i data-id={{$backup->id}} class="fas fa-trash-alt fa-lg btn-backups-del text-danger" title="Excluir"></i>
+								@endif
+							</center>
+						</td>
 					</tr>
 				@endforeach
 			</tbody>
