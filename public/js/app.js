9081301ba6f8ff78da0a58a5ffd26be372fb5100
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-24 11:56:32 -0300

    Recompile

--- public/js/app.js
+++ public/js/app.js
@@ -106672,8 +106672,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\israel.pinheiro\Desktop\Dev\Packager\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\israel.pinheiro\Desktop\Dev\Packager\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\israe\Desktop\Dev\Packager\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\israe\Desktop\Dev\Packager\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
