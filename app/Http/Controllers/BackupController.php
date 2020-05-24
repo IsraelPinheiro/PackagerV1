9d1663618e3d28da9c9fff70e374b24886acee38
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-24 15:26:56 -0300

    Add aditional control logic

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -9,7 +9,9 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 use Auth;
 use Config;
+use Storage;
 use App\Backup;
+use App\ChangeLog;
 
 class BackupController extends Controller{
     /**
@@ -34,17 +36,20 @@ class BackupController extends Controller{
      * @return \Illuminate\Http\Response
      */
     public function create(){
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request){
-        //
+        $host = Config("database.connections.".Config("database.default").".host");
+        $database = Config("database.connections.".Config("database.default").".database");
+        $user = Config("database.connections.".Config("database.default").".username");
+        $password = Config("database.connections.".Config("database.default").".password");
+        $process = new Process(["python",resource_path('python\pybackup.py'),$host,$user,$password,$database]);
+        $process->run();
+        dd($process);
+        //Executes after the command finishes
+        if (!$process->isSuccessful()) {
+            return new ProcessFailedException($process);
+        }
+        else{
+            return True;
+        }
     }
 
     /**
@@ -54,7 +59,19 @@ class BackupController extends Controller{
      * @return \Illuminate\Http\Response
      */
     public function show($id){
-        //
+        $userPermissions = json_decode(Auth::user()->profile->acl_backups);
+        if($userPermissions->read){
+            $backup = Backup::find($id);
+            if($backup){
+                return view('pages.system.backups.show', compact('backup'));
+            }
+            else{
+                return response()->json(['message' => 'Backup não encontrado'],404);
+            }
+        }
+        else{
+            abort(401);
+        }
     }
 
     /**
@@ -64,7 +81,20 @@ class BackupController extends Controller{
      * @return \Illuminate\Http\Response
      */
     public function edit($id){
-        //
+        $userPermissions = json_decode(Auth::user()->profile->acl_backups);
+        if($userPermissions->download){
+            $backup = Backup::find($id);
+            if($backup){
+                return Storage::download($backup->file);
+                //return response()->download("/".$backup->file);
+            }
+            else{
+                return response()->json(['message' => 'Backup não encontrado'],404);
+            }
+        }
+        else{
+            abort(401);
+        }
     }
 
     /**
@@ -85,29 +115,29 @@ class BackupController extends Controller{
      * @return \Illuminate\Http\Response
      */
     public function destroy($id){
-        //
-    }
-
-    /**
-     * Run the backup script
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function backup(){
-        $host = Config("database.connections.".Config("database.default").".host");
-        $database = Config("database.connections.".Config("database.default").".database");
-        $user = Config("database.connections.".Config("database.default").".username");
-        $password = Config("database.connections.".Config("database.default").".password");
-
-        $process = new Process(["python",resource_path('python\pybackup.py'),$host,$user,$password,$database]);
-        $process->run();
-        dd($process);
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            return new ProcessFailedException($process);
+        $userPermissions = json_decode(Auth::user()->profile->acl_backups);
+        if($userPermissions->delete){
+            $backup = Backup::find($id);
+            if($backup){
+                if(env('TRACK_CHANGES', true)){
+                    $log = new ChangeLog;
+                    $log->user_id = Auth::user()->id;
+                    $log->loggable_type = 'Backup';
+                    $log->loggable_id = $id;
+                    $log->target_action = 'delete';
+                    $log->old_data = $backup->toJson();
+                    $log->save();
+                }
+                //TODO: Delete Backup File
+                $backup->delete();
+                return response()->json(['level' => 'success','message' => 'Backup Excluído'],200);
+            }
+            else{
+                return response()->json(['message' => 'Backup Não Encontrado'],404);
+            }
         }
         else{
-            return True;
+            abort(401);
         }
     }
 }
