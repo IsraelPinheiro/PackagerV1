e2e19a78fc63c49d6f1b474ae20d96937247374c
Israel R Pinheiro <israel.pinheiro@live.com>
2020-05-30 22:20:44 -0300

    Update file upload strategy

--- app/Http/Controllers/OutboundPackageController.php
+++ app/Http/Controllers/OutboundPackageController.php
@@ -66,50 +66,29 @@ class OutboundPackageController extends Controller{
         $package->recipient_id = $request->Recipient;
         $package->expires_at = Carbon::parse($request->ExpirationDate)->endOfDay();
         $package->save();
-        //TODO: Enable logging
-        /*
-        if(env('TRACK_CHANGES', true)){
-            $log = new ChangeLog;
-            $log->user_id = Auth::user()->id;
-            $log->loggable_type = 'package';
-            $log->loggable_id = $user->$id;
-            $log->target_action = 'create';
-            $log->old_data = null;
-            $log->save();
-        }*/            
-        return response()->json(['id' => $package->id],200);
-    }
-
-        /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function upload(Request $request){
-        $files = $request->file('Files');
-        dd($request->file('Files'));
-        if(!empty($files)){
-            foreach($files as $f){
-                Storage::put($f);
+        
+        if($request->hasfile('files')){
+            foreach($request->file('files') as $upload){
                 $file = new File;
-                $file->file = Storage::put($f);
-                $file->package_id = $request->PackageId;
-                $file->extension = $f->extension();
+                $file->originalName = $upload->getClientOriginalName();
+                $file->package_id = $package->id;
+                $file->size = $upload->getSize();
+                $file->extension = $upload->extension();
+                $file->file = Storage::putFile('files', $upload);
+                $file->save();
             }
         }
         
         //TODO: Enable logging
-        /*
         if(env('TRACK_CHANGES', true)){
             $log = new ChangeLog;
             $log->user_id = Auth::user()->id;
             $log->loggable_type = 'package';
-            $log->loggable_id = $user->$id;
+            $log->loggable_id = $package->id;
             $log->target_action = 'create';
             $log->old_data = null;
             $log->save();
-        }*/            
+        }
         return response()->json(['message' => 'Pacote Enviado'],200);
     }
 
